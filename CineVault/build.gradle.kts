db2162cad063031c5a150bb796ab8e5d981f8b84
i7m7r8 <imran.neonbati@proton.me>
2026-03-09 04:43:37 +0600

    Update build.gradle.kts

--- CineVault/build.gradle.kts
+++ CineVault/build.gradle.kts
@@ -1,1 +1,10 @@
- 
+version = 1
+
+cloudstream {
+    authorName = "CineVault"
+    description = "CineVault - Movies & TV Shows"
+    language = "en"
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    iconUrl = "https://i7m7r8.github.io/cinevault/icon.png"
+}
