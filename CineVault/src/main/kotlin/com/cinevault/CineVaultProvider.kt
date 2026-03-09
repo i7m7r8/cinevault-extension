c441c81d13e8397a4fac3e6382ac528e83bc0465
i7m7r8 <imran.neonbati@proton.me>
2026-03-09 07:54:56 +0600

    Update CineVaultProvider.kt

--- CineVault/src/main/kotlin/com/cinevault/CineVaultProvider.kt
+++ CineVault/src/main/kotlin/com/cinevault/CineVaultProvider.kt
@@ -1,12 +1,13 @@
 package com.cinevault
 
 import android.content.Context
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.lagradost.cloudstream3.*
-import com.lagradost.cloudstream3.metaproviders.TmdbProvider
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import com.lagradost.cloudstream3.utils.*
 import com.lagradost.cloudstream3.utils.AppUtils.parseJson
+import com.lagradost.cloudstream3.utils.AppUtils.toJson
 import com.lagradost.cloudstream3.utils.ExtractorLinkType
 
 @CloudstreamPlugin
@@ -16,16 +17,16 @@ class CineVaultPlugin : Plugin() {
     }
 }
 
-class CineVaultProvider : TmdbProvider() {
+class CineVaultProvider : MainAPI() {
     override var name = "CineVault"
-    override var mainUrl = "https://www.themoviedb.org"
+    override var mainUrl = "https://cinevault-api.imrannn68d-de2.workers.dev"
     override var lang = "en"
     override val hasMainPage = true
     override val supportedTypes = setOf(TvType.Movie, TvType.TvSeries, TvType.Torrent)
 
-    private val workerApi = "https://cinevault-api.imrannn68d-de2.workers.dev"
     private val tmdbApi = "https://api.themoviedb.org/3"
     private val apiKey = "4ef0d7355d9ffb5151e987764708ce96"
+    private val workerApi = "https://cinevault-api.imrannn68d-de2.workers.dev"
 
     override val mainPage = mainPageOf(
         "$tmdbApi/trending/all/week?api_key=$apiKey" to "Trending",
@@ -35,16 +36,96 @@ class CineVaultProvider : TmdbProvider() {
         "$tmdbApi/tv/top_rated?api_key=$apiKey" to "Top Rated TV Shows"
     )
 
+    private fun getImageUrl(link: String?): String? {
+        if (link == null) return null
+        return if (link.startsWith("/")) "https://image.tmdb.org/t/p/w500$link" else link
+    }
+
+    private fun getType(t: String?): TvType {
+        return when (t) {
+            "movie" -> TvType.Movie
+            else -> TvType.TvSeries
+        }
+    }
+
+    override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
+        val type = if (request.data.contains("/movie")) "movie" else "tv"
+        val res = app.get("${request.data}&page=$page").parsedSafe<Results>()
+            ?: throw ErrorLoadingException("Invalid Json Response")
+        val home = res.results?.mapNotNull { it.toSearchResponse(type) } ?: emptyList()
+        return newHomePageResponse(request.name, home)
+    }
+
+    private fun Media.toSearchResponse(type: String? = null): SearchResponse? {
+        val mediaType = type ?: this.media_type ?: return null
+        if (mediaType == "person") return null
+        val t = title ?: name ?: original_title ?: original_name ?: return null
+        val poster = getImageUrl(poster_path)
+        val data = Data(id = id, type = mediaType).toJson()
+        return if (mediaType == "movie") {
+            newMovieSearchResponse(t, data, TvType.Movie) { this.posterUrl = poster }
+        } else {
+            newTvSeriesSearchResponse(t, data, TvType.TvSeries) { this.posterUrl = poster }
+        }
+    }
+
+    override suspend fun search(query: String): List<SearchResponse> {
+        val res = app.get("$tmdbApi/search/multi?api_key=$apiKey&query=$query")
+            .parsedSafe<Results>() ?: return emptyList()
+        return res.results?.mapNotNull { it.toSearchResponse() } ?: emptyList()
+    }
+
+    override suspend fun load(url: String): LoadResponse? {
+        val data = parseJson<Data>(url)
+        val type = getType(data.type)
+        val isMovie = type == TvType.Movie
+
+        val resUrl = if (isMovie)
+            "$tmdbApi/movie/${data.id}?api_key=$apiKey&append_to_response=external_ids"
+        else
+            "$tmdbApi/tv/${data.id}?api_key=$apiKey&append_to_response=external_ids,seasons"
+
+        if (isMovie) {
+            val res = app.get(resUrl).parsedSafe<MovieDetail>() ?: return null
+            val imdbId = res.external_ids?.imdb_id ?: return null
+            val title = res.title ?: res.original_title ?: return null
+            return newMovieLoadResponse(title, url, TvType.Movie, LinkData(imdbId, null, null).toJson()) {
+                this.posterUrl = getImageUrl(res.poster_path)
+                this.plot = res.overview
+                this.year = res.release_date?.take(4)?.toIntOrNull()
+            }
+        } else {
+            val res = app.get(resUrl).parsedSafe<TvDetail>() ?: return null
+            val imdbId = res.external_ids?.imdb_id ?: return null
+            val title = res.name ?: res.original_name ?: return null
+            val seasons = res.seasons?.filter { (it.season_number ?: 0) > 0 } ?: emptyList()
+            val episodes = seasons.flatMap { season ->
+                val sNum = season.season_number ?: return@flatMap emptyList()
+                val epCount = season.episode_count ?: 0
+                (1..epCount).map { ep ->
+                    newEpisode(LinkData(imdbId, sNum, ep).toJson()) {
+                        this.season = sNum
+                        this.episode = ep
+                    }
+                }
+            }
+            return newTvSeriesLoadResponse(title, url, TvType.TvSeries, episodes) {
+                this.posterUrl = getImageUrl(res.poster_path)
+                this.plot = res.overview
+            }
+        }
+    }
+
     override suspend fun loadLinks(
         data: String,
         isCasting: Boolean,
         subtitleCallback: (SubtitleFile) -> Unit,
         callback: (ExtractorLink) -> Unit
     ): Boolean {
-        val tmdbLink = parseJson<TmdbLink>(data)
-        val imdbId = tmdbLink.imdbID ?: return false
-        val season = tmdbLink.season
-        val episode = tmdbLink.episode
+        val linkData = parseJson<LinkData>(data)
+        val imdbId = linkData.imdbId ?: return false
+        val season = linkData.season
+        val episode = linkData.episode
         val isMovie = season == null
 
         val streamUrl = if (isMovie) "$workerApi/streams/$imdbId"
@@ -60,10 +141,7 @@ class CineVaultProvider : TmdbProvider() {
                 "&tr=udp://exodus.desync.com:6969/announce" +
                 "&tr=udp://tracker.torrent.eu.org:451/announce"
             callback(
-                newExtractorLink(
-                    name, title, magnet,
-                    ExtractorLinkType.MAGNET
-                ) {
+                newExtractorLink(name, title, magnet, ExtractorLinkType.MAGNET) {
                     this.referer = ""
                     this.quality = getQualityFromName(title)
                 }
@@ -80,13 +158,40 @@ class CineVaultProvider : TmdbProvider() {
         return true
     }
 
-    data class TmdbLink(
-        val imdbID: String? = null,
-        val tmdbID: Int? = null,
-        val episode: Int? = null,
-        val season: Int? = null,
-        val movieName: String? = null
+    data class Data(val id: Int? = null, val type: String? = null)
+    data class LinkData(val imdbId: String? = null, val season: Int? = null, val episode: Int? = null)
+    data class Results(val results: List<Media>? = null)
+    data class Media(
+        val id: Int? = null,
+        val title: String? = null,
+        val name: String? = null,
+        val original_title: String? = null,
+        val original_name: String? = null,
+        val poster_path: String? = null,
+        val media_type: String? = null,
+        val overview: String? = null,
+        val release_date: String? = null
     )
+    data class MovieDetail(
+        val id: Int? = null,
+        val title: String? = null,
+        val original_title: String? = null,
+        val poster_path: String? = null,
+        val overview: String? = null,
+        val release_date: String? = null,
+        val external_ids: ExternalIds? = null
+    )
+    data class TvDetail(
+        val id: Int? = null,
+        val name: String? = null,
+        val original_name: String? = null,
+        val poster_path: String? = null,
+        val overview: String? = null,
+        val seasons: List<Season>? = null,
+        val external_ids: ExternalIds? = null
+    )
+    data class Season(val season_number: Int? = null, val episode_count: Int? = null)
+    data class ExternalIds(val imdb_id: String? = null)
     data class TorrentioStream(
         val title: String? = null,
         val name: String? = null,
